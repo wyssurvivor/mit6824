d0cfc244e441c15a2f907b8d276a0828596307fd
wyssurvivor <937423966@qq.com>
2017-01-07 23:03:49 +0800

    update schedule

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -24,5 +24,62 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	finish:=make(chan bool,ntasks)
+	for i:=0;i<ntasks;i++ {
+		index:=i
+		res:=false
+		go func() {
+			for !res {
+				worker:=<-mr.registerChannel
+				arg:=DoTaskArgs{mr.jobName,mr.files[index],phase,index,nios}
+				res=call(worker,"Worker.DoTask",&arg,new(struct{}))
+				if res {
+					mr.registerChannel<-worker
+					fmt.Printf("%d stick worker %s backinto registerChannel \n",index,worker)
+					finish<- true
+					fmt.Printf("%d stick finish channel \n",index)
+
+				} else {
+					fmt.Printf("phase %s,task %d failed",phase,index)
+				}
+			}
+		}()
+	}
+
+	for count:=0;count<ntasks;count++ {
+		<-finish
+		fmt.Printf("%d task finished",count+1)
+	}
+
+
+	//signals := make(chan bool, ntasks)
+	//for i:=0; i < ntasks; i++{
+	//
+	//	// must save i to a local variable
+	//	taskIdx := i
+	//	go func(){
+	//		ok := false
+	//		for !ok {
+	//			// get a free worker
+	//			worker := <-mr.registerChannel
+	//			// rpc call to finish job
+	//			args := DoTaskArgs{mr.jobName, mr.files[taskIdx], phase, taskIdx, nios}
+	//			ok = call(worker, "Worker.DoTask", &args, new(struct{}))
+	//			if !ok {
+	//				fmt.Printf("fail: %v %v tasks (%d I/Os)\n", taskIdx, phase, nios)
+	//			}else {
+	//				signals <- true
+	//				// return the free worke
+	//				mr.registerChannel <- worker
+	//			}
+	//		}
+	//	}()
+	//}
+	//
+	//// sync
+	//for i:=0; i < ntasks; i++{
+	//	<- signals
+	//}
+
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
